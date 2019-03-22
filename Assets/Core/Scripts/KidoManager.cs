bfbf8781e2efebc6008d17ddae5673f814d33d2c
1pp0 <>
2019-03-22 17:34:36 +0900

    feat: Speech Recognition

---
+++ Assets/Core/Scripts/KidoManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UniSpeech;
+
+public class KidoManager : MonoBehaviour, ISpeechRecognizer
+{    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (SpeechRecognizer.StartRecord())
+            {
+                Debug.Log("StartRecord");
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (SpeechRecognizer.StopRecord())
+            {
+                Debug.Log("StopRecord");
+            }
+        }
+    }
+
+    public void OnRecognized(string transcription)
+    {
+        Debug.Log($"Recognized: {transcription}");
+    }
+
+    public void OnError(string description)
+    {
+        Debug.LogWarning($"Error: {description}");
+    }
+
+    public void OnAuthorized()
+    {
+        Debug.Log("Authorized");
+    }
+
+    public void OnUnauthorized()
+    {
+        Debug.LogWarning("Unauthorized");
+    }
+
+    public void OnAvailable()
+    {
+        Debug.Log("Available");
+    }
+
+    public void OnUnavailable()
+    {
+        Debug.LogWarning("Unavailable");
+    }
+}
