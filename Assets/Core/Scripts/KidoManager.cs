483e7a1654535a2409c67ddf34bcc28e5d6a507f
1pp0 <>
2019-03-25 00:28:42 +0900

    feat: UI & black coffin size

--- Assets/Core/Scripts/KidoManager.cs
+++ Assets/Core/Scripts/KidoManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UniSpeech;
 using UnityEngine.UI;
 
@@ -8,6 +10,19 @@ public class KidoManager : MonoBehaviour, ISpeechRecognizer
     [SerializeField] private Text transcriptionText;
     [SerializeField] private Transform focusedSquare;
     private bool inAria;
+    
+    private const string Spell90 = "滲み出す混濁の紋章\n不遜なる狂気の器\n湧き上がり・否定し・痺れ・瞬き\n眠りを妨げる\n爬行する鉄の王女\n絶えず自壊する泥の人形\n結合せよ 反発せよ\n地に満ち 己の無力を知れ";
+    private readonly char[] twoWordList =
+    {
+        '滲', '出', '混', '濁', '紋', '章', '不', '遜', '狂', '気', 
+        '器', '湧', '上', '否', '定', '痺', '瞬', '眠', '妨', '爬',
+        '行', '鉄', '王', '女', '絶', '自', '壊', '泥', '人', '形', 
+        '結', '合', '反', '発', '地', '満', '己', '無', '力', '知'
+    };
+    private readonly Dictionary<string, string> replaceWordList = new Dictionary<string, string>()
+    {
+        {"にじみ", "滲み"}
+    };
     
     private void Update()
     {
@@ -25,7 +40,7 @@ public class KidoManager : MonoBehaviour, ISpeechRecognizer
             {
                 Debug.Log("StopRecord");
                 inAria = false;
-                transcriptionText.text = "";
+                transcriptionText.text = Spell90;
             }
         }
     }
@@ -33,25 +48,45 @@ public class KidoManager : MonoBehaviour, ISpeechRecognizer
     public void OnRecognized(string transcription)
     {
         Debug.Log($"Recognized: {transcription}");
-        transcriptionText.text = transcription;
+        foreach (var pair in replaceWordList)
+        {
+            transcription = transcription.Replace(pair.Key, pair.Value);
+        }
+
+        var score = twoWordList.Count(transcription.Contains);
         if (transcription.Contains("黒棺"))
         {
-            CreateBlackCoffin();
+            CreateBlackCoffin(score);
         }
+        
+        var tmp = "";
+        foreach (var word in Spell90)
+        {
+            if (twoWordList.Contains(word) && transcription.Contains(word))
+            {
+                tmp += $"<color=red>{word}</color>";
+            }
+            else
+            {
+                tmp += word;
+            }
+        }
+        transcriptionText.text = tmp;
     }
     
-    private void CreateBlackCoffin()
+    private void CreateBlackCoffin(int score)
     {
         Debug.Log("CreateBlackCoffin");
         if (!inAria) return;
         inAria = false;
-        Instantiate(blackCoffin, focusedSquare.position, focusedSquare.rotation);
+        var bc = Instantiate(blackCoffin, focusedSquare.position, focusedSquare.rotation);
+        bc.transform.localScale *= score / 10f + 1;
     }
 
     public void OnError(string description)
     {
         Debug.LogWarning($"Error: {description}");
-        transcriptionText.text = "";
+        transcriptionText.text = Spell90;
     }
 
     public void OnAuthorized()
